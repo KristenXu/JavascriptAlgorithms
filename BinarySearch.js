286976e04e7512f164e708d2ebb3fab81ea8ccc3
Chainn <578579786@qq.com>
2016-11-24 11:14:46 +0800

    add binarySearch

---
+++ BinarySearch.js
@@ -0,0 +1,19 @@
+function binarySearch(target, array) {
+  var low = 0,
+    high = array.length - 1,
+    mid, midElement;
+  while(low <= high){
+    mid = Math.floor((low + high) / 2);
+    midElement = array[mid];
+    if (target > midElement) {
+      low = mid + 1;
+    }else if (target < midElement){
+      high = mid - 1;
+    }else{
+      console.log('target index is : ' + mid);
+      return ;
+    }
+  }
+
+  return -1;
+}
